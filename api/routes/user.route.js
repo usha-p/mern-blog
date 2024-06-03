684af1bb1f4235af360b87e13547424147d3d866
usha-p <ushashreepolarouthu@gmail.com>
2024-06-03 22:03:43 +0530

    update user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import {test} from '../controllers/user.controller.js'
+import { updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utlis/verifyUser.js';
 const router=express.Router();
 
 router.get('/test',test)
+router.put('/update/:userId',verifyToken,updateUser)
 export default router;
--- api/utlis/error.js
+++ api/utlis/error.js
@@ -1,5 +1,6 @@
 export const errorHandler=(statusCode,message)=>{
     const error=new Error()
     error.message=message
+    error.statusCode=statusCode
     return error
 }
