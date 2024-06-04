115f1d42244181bf22ad8e3de6255bc5d8e173d4
usha-p <ushashreepolarouthu@gmail.com>
2024-06-04 22:42:20 +0530

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import {test} from '../controllers/user.controller.js'
+import {deleteUser, test} from '../controllers/user.controller.js'
 import { updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utlis/verifyUser.js';
 const router=express.Router();
 
 router.get('/test',test)
 router.put('/update/:userId',verifyToken,updateUser)
+router.delete('/delete/:userId',verifyToken,deleteUser);
+
 export default router;
