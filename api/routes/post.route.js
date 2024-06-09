d7b900ca1d96401a5017836c2f3a046ec6845da9
usha-p <ushashreepolarouthu@gmail.com>
2024-06-09 14:08:27 +0530

    add delete post functionality to dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { verifyToken } from "../utlis/verifyUser.js";
-import { create, getPosts } from "../controllers/post.controller.js";
+import { create, deletepost, getPosts } from "../controllers/post.controller.js";
 const router=express.Router();
 
 router.post('/create',verifyToken,create)
 router.get('/getposts',getPosts)
+router.delete('/deletepost/:postId/:userId',verifyToken,deletepost)
 export default router;
