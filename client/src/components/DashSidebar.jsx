c2e7eb4ff9d836dff0c9994bb449532027f0ce26
usha-p <ushashreepolarouthu@gmail.com>
2024-06-12 22:01:12 +0530

    add comment section to user dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import  {Sidebar} from 'flowbite-react'
-import {HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
+import {HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
 import { useEffect,useState } from 'react';
 import {Link, useLocation} from 'react-router-dom'
 import { signOutSuccess } from '../redux/user/userSlice';
@@ -60,12 +60,21 @@ dispatch(signOutSuccess())
 
            )}
            {currentUser.isAdmin &&(
+            <>
              <Link to='/dashboard?tab=users'>
              <Sidebar.Item active={tab==='users'} icon={HiOutlineUserGroup
              } as='div'>
                Users
              </Sidebar.Item>
            </Link>
+           <Link to='/dashboard?tab=comments'>
+             <Sidebar.Item active={tab==='comments'} icon={HiAnnotation
+             } as='div'>
+               Comments
+             </Sidebar.Item>
+           </Link>
+           </>
+            
            
            )}
             <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignOut}>
